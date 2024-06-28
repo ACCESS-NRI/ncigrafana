d1d36eaeec240bace51683f7b68b8e66053edcc1
utkarsh gupta <utkarshgupta.rtm@gmail.com>
2024-06-28 11:01:14 +1000

    fixing read_sql issue with pandas 2.2.0

--- ncigrafana/UsageDataset.py
+++ ncigrafana/UsageDataset.py
@@ -421,11 +421,10 @@ class ProjectDataset(object):
 
         # Pivot makes columns of all the individuals, rows are indexed by date
         try:
-            df = pd.read_sql_query(qstring.format(namefield=name_sql,
+            df = pd.DataFrame(data=list(self.db.query(qstring.format(namefield=name_sql,
                                                   datafield=datafield,
                                                   start=startdate,
-                                                  end=enddate),
-                                                  self.db.executable).pivot_table(index='Date',
+                                                  end=enddate)))).pivot_table(index='Date',
                                                                                   columns='Name',
                                                                                   fill_value=0)
         except:
@@ -470,14 +469,14 @@ class ProjectDataset(object):
 
         # Pivot makes columns of all the individuals, rows are indexed by date
         try:
-            df = pd.read_sql_query(qstring.format(namefield=name_sql,
+            df = pd.DataFrame(data=list(self.db.query(qstring.format(namefield=name_sql,
                                                   datafield=datafield,
                                                   table=table,
                                                   project_id=project_id,
                                                   storagepoint_id=storagepoint_id,
                                                   start=startdate,
-                                                  end=enddate), 
-                                   self.db.executable).pivot_table(index='Date', columns='Name', fill_value=0)
+                                                  end=enddate)))).pivot_table(index='Date', 
+                                                                              columns='Name', fill_value=0)
         except:
             print("No data available for {}".format(storagepoint))
             return None
